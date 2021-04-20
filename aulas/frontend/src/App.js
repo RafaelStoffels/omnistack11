4d038e7fd1d4fdd683cf0f032564b2a2ca26bf36
Rafael Stoffels <stoffels.rafael@gmail.com>
2021-04-20 17:01:33 -0300

    Created Logon Page
    Created Logon Page

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,9 +1,24 @@
-
+//import React, { useState } from 'react';
+import React from 'react';
+import './global.css';
+import Logon from './pages/Logon';
 
 // Is a function that returns the HTML, so it is a JSX (Javascript XML)
 function App() { 
+/*  let [counter, setCounter] = useState(0);
+
+  // useState => array [valor, funcaoDeAtualizacao] - never assign a value manually like counter += 1;, always use the setCounter of the useState because all components will be refreshed on screen automatically ;)
+
+  function increment() {
+    setCounter(counter += 1);
+  }*/
+
   return (
-    <h1>Hello World</h1>
+    <Logon />
+   /* <div>
+        <Header>Contador: {counter}</Header>
+        <button onClick={increment}>Incrementar</button>
+    </div>*/
   );
 }
 
