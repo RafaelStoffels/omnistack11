31cd678201c4485fb810eaaa4c3cf3905fe46981
RafaelStoffels <53713740+RafaelStoffels@users.noreply.github.com>
2021-04-19 21:55:33 -0300

    Unnecessary structures removed
    Unnecessary structures removed

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+// Is a function that returns the HTML, so it is a JSX (Javascript XML)
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello World</h1>
   );
 }
 
