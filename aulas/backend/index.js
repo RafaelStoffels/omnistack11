dbf4ab51309977dfea09a26aca9ec3791a20fc3b
RafaelStoffels <53713740+RafaelStoffels@users.noreply.github.com>
2021-05-02 16:19:35 -0300

    API Integration front-back (Axious)
    API Integration front-back (Axious)

--- aulas/backend/index.js
+++ aulas/backend/index.js
@@ -3,12 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  //  return response.send("Hello World!");
     return response.json({
-    evento: "Hello World",
-    aluno: "Rafael Stoffels",
-    })
+        evento: "Semana Omnistack 11",
+        aluno: "Rafael Stoffels"
+    });
 });
 
 app.listen(3333);
-
--- aulas/frontend/src/pages/Register/index.js
+++ aulas/frontend/src/pages/Register/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
+import api from '../../services/api';
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function Register() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [whatsapp, setWhatsapp] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+
+    function handleRegister(e){
+        e.preventDefault();        //nao recarrega a pagina qdo efetua um submit
+
+        const data = {
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        };
+    }
+
     return(
         <div className="register-container">
             <div className="content">
@@ -22,14 +41,38 @@ export default function Register() {
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder="NGO´s Name" />
-                    <input type="email" placeholder="E-mail" />
-                    <input placeholder="WhatsApp" />
+                <form onSubmit={handleRegister}>
+                    <input 
+                        placeholder="NGO´s Name" 
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
+
+                    <input 
+                        type="email" 
+                        placeholder="E-mail" 
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+
+                    <input 
+                        placeholder="WhatsApp" 
+                        value={whatsapp}
+                        onChange={e => setWhatsapp(e.target.value)}
+                    />
 
                     <div className="input-group">
-                        <input placeholder="City" />
-                        <input placeholder="State" style={{width: 80}} />
+                        <input 
+                            placeholder="City" 
+                            value={city}
+                            onChange={e => setCity(e.target.value)}
+                        />
+                        <input 
+                            placeholder="State" 
+                            style={{width: 80}} 
+                            value={uf}
+                            onChange={e => setUf(e.target.value)}
+                        />
                     </div>
 
                     <button className="button" type="submit">Register</button>
