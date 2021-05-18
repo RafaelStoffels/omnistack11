ce513a3508fbcaa991c7cfe3e53c78cc9a28abe2
Rafael <stoffels.rafael@gmail.com>
2021-05-17 21:15:54 -0300

    Static List
    Static List

--- aulas/mobile/App.js
+++ aulas/mobile/App.js
@@ -1,12 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World </Text>
-      <StatusBar style="auto" />
-    </View>
+    <Routes />
   );
 }
