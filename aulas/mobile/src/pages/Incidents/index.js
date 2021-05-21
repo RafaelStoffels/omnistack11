5a941a2d036c39848482762d9343352305cdf6b8
Rafael <stoffels.rafael@gmail.com>
2021-05-20 22:28:42 -0300

    Mobile details and pagination

--- aulas/mobile/src/pages/Incidents/index.js
+++ aulas/mobile/src/pages/Incidents/index.js
@@ -11,17 +11,35 @@ import styles from './styles';
 
 export default function Incidents() {
     const [incidents, setIncidents] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+
     const navigation = useNavigation();
 
-    function navigateToDetail() {
-        navigation.navigate('Detail');
+    function navigateToDetail(incident) {
+        navigation.navigate('Detail', { incident });
     }
 
-
     async function loadIncidents(){
-        const response = await api.get('incidents');
+        if (loading) {
+            return;
+        }
 
-        setIncidents(response.data);
+        if (total > 0 && incidents.lennght == total){
+            return;
+        }
+
+        setLoading(true);
+
+        const response = await api.get('incidents', {
+            params: {page}
+        });
+
+        setIncidents([...incidents, ...response.data]);
+        setTotal(response.headers['x-total-count']);
+        setPage(page + 1);
+        setLoading(false);
     }
 
     useEffect(() => {
@@ -33,7 +51,7 @@ export default function Incidents() {
             <View style={styles.header} >
                 <Image source={logoImg} />
                 <Text style={styles.headerText}>
-                    Total de <Text style={styles.headerTextBold}>0 casos </Text>
+                    Total de <Text style={styles.headerTextBold}>{total} casos </Text>
                 </Text>
             </View>
 
@@ -45,6 +63,8 @@ export default function Incidents() {
                 View style={styles.incident}
                 keyExtractor={incident => String(incident.id)}
                 showsVerticalScrollIndicator={false}
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.2}
                 renderItem={({ item: incident}) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
@@ -54,11 +74,15 @@ export default function Incidents() {
                         <Text style={styles.incidentValue}>{incident.title}</Text>
     
                         <Text style={styles.incidentProperty}>VALOR:</Text>
-                        <Text style={styles.incidentValue}>{incident.value}</Text>
+                        <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR', {
+                            style: 'currency', 
+                            currency: 'BRL' 
+                            }).format(incident.value)}
+                        </Text>
     
                         <TouchableOpacity
                             style={styles.detailsButton}
-                            onPress={navigateToDetail}
+                            onPress={() => navigateToDetail(incident)}
                         >
                             <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
                             <Feather name="arrow-right" size={16} color="#E02041" />
--- aulas/mobile/src/services/api.js
+++ aulas/mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'exp://192.168.0.111:19000'
+    baseURL: 'http://192.168.0.111:3333'
 });
 
 export default api;
